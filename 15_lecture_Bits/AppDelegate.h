28772d9f23f348757d87bb505625a420d4263941
Slava <rtootik@gmail.com>
2017-03-19 10:53:55 +0300

    Initial Commit

---
+++ 15_lecture_Bits/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  15_lecture_Bits
+//
+//  Created by Slava on 19.03.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
